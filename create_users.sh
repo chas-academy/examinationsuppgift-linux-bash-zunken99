e04991559cf3cb1883643ab36c37fa5639e7c669
zunken99 <isakoaderu@gmail.com>
2026-05-19 19:35:48 +0000

    klar 1.1

--- create_users.sh
+++ create_users.sh
@@ -62,15 +62,15 @@ do
 	
 	{
 		echo "Välkommen $username"
-		echo " "
-		echo "Andra användare här: "
+		echo ""
+		echo "Andra användare: "
 		echo "$existing_users"
 	} > "$home_dir/welcome.txt"
 
 	#Sätt ägare och rättigheter på filen
 	
 	chown "$username:$username" "$home_dir/welcome.txt"
-	chmod 600 "$home_dir/welcome.txt"
+	chmod 400 "$home_dir/welcome.txt"
 	
 	echo "Användare $username skapad."
 done
